a188a3d02a9d4afba1ce0e95dce7e9cec292eef7
Natalia da Silva Reis <nataliadasilvareis@gmail.com>
2026-02-11 09:41:42 -0300

    Finalizados os exercícios 01 e 02 da lista 03.

---
+++ Algoritmos e técnicas de programação/Lista 03/Exercício 01/Exercício 01/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Exercício_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Leia os elementos de uma matriz quadrada
+            // e mostre os elementos da diagonal principal.
+
+            int tamanho;
+            int[,] matriz;
+
+            Console.Write("Informe o tamanho da matriz quadrada: ");
+            tamanho = int.Parse(Console.ReadLine());
+            matriz = new int[tamanho, tamanho];
+
+            Console.WriteLine();
+            Console.WriteLine("Populando a matriz:");
+            for (int i = 0; i < tamanho; i++)
+            {
+                for (int j = 0; j < tamanho; j++)
+                {
+                    Console.Write($"Matriz[{i}, {j}]: ");
+                    matriz[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+
+            Console.WriteLine();
+            Console.Write("Diagonal principal: ");
+            for (int i = 0; i < tamanho; i++)
+            {
+                for (int j = 0; j < tamanho; j++)
+                {
+                    if (i == j)
+                    {
+                        Console.Write($"{matriz[i, j]}  ");
+                    }
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 # 🖥️ Exercícios C#
 
-Repositório com listas de exercícios do curso de **Sistemas de Informação**.
+Repositório com exercícios do curso de **Sistemas de Informação**.
 
 ## 📚 Listas
 - **Lista 01** → Variáveis, Operadores e Estruturas Sequenciais
 - **Lista 02** → Estruturas Condicionais - If / Else
+- **Lista 03** → Arranjos Multidimensionais - Matriz
+
+## 📚 300 Ideias Para Programar Computadores
+- **Básicos** → 1.1.10
 
 ---
 
